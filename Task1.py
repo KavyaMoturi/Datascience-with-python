4a6a033bfc663248242792124f9091171e64b35c
KavyaMoturi <142881626+KavyaMoturi@users.noreply.github.com>
2024-05-03 16:19:24 +0530

    Create Task1.py

---
+++ Task1.py
@@ -0,0 +1,38 @@
+# Creating a list
+my_list = [1, 2, 3, 4, 5]
+
+# Adding an element to the list
+my_list.append(6)
+
+# Removing an element from the list
+my_list.remove(3)
+
+# Modifying an element in the list
+my_list[0] = 10
+
+print("List:", my_list)
+
+# Creating a dictionary
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+
+# Adding a key-value pair to the dictionary
+my_dict['d'] = 4
+
+# Removing a key-value pair from the dictionary
+del my_dict['b']
+
+# Modifying a value in the dictionary
+my_dict['a'] = 10
+
+print("Dictionary:", my_dict)
+
+# Creating a set
+my_set = {1, 2, 3, 4, 5}
+
+# Adding an element to the set
+my_set.add(6)
+
+# Removing an element from the set
+my_set.remove(3)
+
+print("Set:", my_set)
